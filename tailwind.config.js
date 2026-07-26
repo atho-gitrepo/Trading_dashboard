940910547557b093acb4736636fee06ae836168e
atho-gitrepo <aungthuheinoo1992@gmail.com>
2026-07-26 13:35:30 +0630

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
@@ -28,4 +31,4 @@ export default {
     },
   },
   plugins: [],
-};
+}
