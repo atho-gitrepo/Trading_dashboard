683a9c922a4b91743618d96d2b4c4fc70262c71a
atho-gitrepo <aungthuheinoo1992@gmail.com>
2026-07-26 16:35:47 +0630

    Update BottomNav.tsx

--- src/components/Navigation/BottomNav.tsx
+++ src/components/Navigation/BottomNav.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 
-export const BottomNav: React.FC = () => {
+export const BottomNav = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
