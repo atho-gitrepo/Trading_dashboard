5a949a01496a735507a7198647ecb1c3bdc907f2
atho-gitrepo <aungthuheinoo1992@gmail.com>
2026-07-26 17:48:14 +0630

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'terser',
     sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore'],
-        },
-      },
-    },
   },
   server: {
     port: 3000,
